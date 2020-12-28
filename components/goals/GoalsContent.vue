1830bae2073771054983f7bd17c84749e13596bf
Quang Pham <quanghuy25899@gmail.com>
2020-12-28 13:37:25 -0800

    updates

--- components/goals/GoalsContent.vue
+++ components/goals/GoalsContent.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <div id="future-goals" class="is-background-primary is-title">FUTURE GOALS</div>
-    <div>
-      - Improve front-end and back-end programming skills.
-      <br />- Learn more front-end programming languages, starting with VueJS.
-      <br />- Become a full-stack web developer.
+    <div id="future-goals" class="is-background-primary is-title">
+      FUTURE GOALS
     </div>
+    <div>- Improve front-end and back-end programming skills.</div>
+    <div>
+      - Learn more front-end programming languages, starting with VueJS.
+    </div>
+    <div>- Become a full-stack web developer.</div>
   </div>
 </template>
 
